92772930576c926de3248e0e478c4aff43aa39a7
hj235 <hongjing235@gmail.com>
2024-06-01 19:37:56 +0800

    Established connection to MongoDB database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const app = express();
 const dotenv = require('dotenv').config();
 const cors = require('cors');
+const {mongoose} = require('mongoose');
 
-const app = express();
+// connect to database
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log('Database connected'))
+.catch((err) => console.log('Error on initial connection to database', err));
+// to handle errors after initial connection is established
+mongoose.connection.on('error', err => {
+    console.log('Error on connection to database', err)
+});
 
-app.use('/', require('./routes/routes'))
+// middleware
+app.use(express.json());
+app.use('/', require('./routes/routes'));
 
+// listen on port 8000
 const port = 8000;
 app.listen(port, () => console.log(`Sever running on port ${port}`));
