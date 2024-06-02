0211de975c531a45733576b9a87b2b75a89b0987
kohtw <tingweipro@gmail.com>
2024-06-02 15:44:39 +0800

    Added some comments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,15 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
-
-// pages
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
-
-// components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import axios from 'axios';
 
 // axios stuff to make life easier
-import axios from 'axios';
-// simplifies having to type out the server url multiple times
-axios.defaults.baseURL = 'http://localhost:8000';
-// allows cookies on client side
-axios.defaults.withCredentials = true; 
+axios.defaults.baseURL = 'http://localhost:8000'; // simplifies having to type out the server url multiple times
+axios.defaults.withCredentials = true; // allows cookies on client side
 
 function App() {
 
