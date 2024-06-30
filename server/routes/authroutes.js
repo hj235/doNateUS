dca42e6ef0a2e523613df27f54ca33c81a82d818
hj235 <hongjing235@gmail.com>
2024-06-30 17:07:42 +0800

    Functionality: getUser API & change UserContext Syncs the user with the database whenever UserContext loads

--- server/routes/authroutes.js
+++ server/routes/authroutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {registerUser, loginUser, logoutUser, editUser } = require('../controllers/authController');
+const {registerUser, loginUser, logoutUser, editUser, getUser } = require('../controllers/authController');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.patch('/api/user/edit/:id', editUser);
+router.get('/api/user/get/:id', getUser);
 
 module.exports = router;
